c34595574f7f67839720d16cd5cd78e51f1fb218
Nikulin Max <minikulin@avito.ru>
2017-05-17 21:00:17 +0300

    Initial commit.

---
+++ Occupation/Occupation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Occupation
+//
+//  Created by Никулин Максим on 17/05/2017.
+//  Copyright © 2017 Никулин Максим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Occupation/Occupation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Occupation
+//
+//  Created by Никулин Максим on 17/05/2017.
+//  Copyright © 2017 Никулин Максим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
