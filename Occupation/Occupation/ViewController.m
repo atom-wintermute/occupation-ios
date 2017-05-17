f23bab5d5c8d84c06b9692f7f9bf817cf35f8059
Nikulin Max <minikulin@avito.ru>
2017-05-17 22:36:07 +0300

    Add buttons setup.

--- Occupation/Occupation/ViewController.m
+++ Occupation/Occupation/ViewController.m
@@ -8,12 +8,17 @@
 
 #import "ViewController.h"
 
-static CGFloat const AONavigationBarHeigth = 44.0;
 static CGFloat const AOStatusBarHeight = 20.0;
 
 static NSUInteger const AOFieldSize = 18;
+static NSUInteger const AOButtonCount = 5;
 static CGFloat const AOFieldMargin = 10.0;
+
 static CGFloat const AOCellSpacing = 1.0;
+
+static CGFloat const AOButtonOffset = 15.0;
+static CGFloat const AOButtonHeight = 44.0;
+static CGFloat const AOButtonSpacing = 1.0;
 
 @implementation ViewController
 
@@ -24,6 +29,7 @@ static CGFloat const AOCellSpacing = 1.0;
     
     [self setupView];
     [self setupCells];
+    [self setupButtons];
 }
 
 #pragma mark - View setup
@@ -33,8 +39,7 @@ static CGFloat const AOCellSpacing = 1.0;
 }
 
 - (void)setupCells {
-    CGFloat cellSize = (CGRectGetWidth(UIScreen.mainScreen.bounds) - AOFieldMargin * 2) / AOFieldSize - AOCellSpacing;
-    
+    CGFloat cellSize = [self cellSize];
     CGFloat leftPosition = AOFieldMargin;
     CGFloat topPosition = AOFieldMargin + AOStatusBarHeight;
     
@@ -45,9 +50,25 @@ static CGFloat const AOCellSpacing = 1.0;
             UIView *cell = [[UIView alloc] initWithFrame:frame];
             cell.backgroundColor = [self generateRandomColor];
             [self.view addSubview:cell];
+            
             leftPosition += cellSize + AOCellSpacing;
         }
         topPosition += cellSize + AOCellSpacing;
+    }
+}
+
+- (void)setupButtons {
+    CGFloat buttonWidth = (CGRectGetWidth(UIScreen.mainScreen.bounds) - AOFieldMargin * 2) / AOButtonCount - AOButtonSpacing;
+    CGFloat topPosition = AOFieldMargin + AOStatusBarHeight + AOFieldSize * ([self cellSize] + AOCellSpacing) + AOButtonOffset;
+    CGFloat leftPosition = AOFieldMargin;
+    
+    for (NSUInteger i = 0; i < AOButtonCount; ++i) {
+        CGRect frame = CGRectMake(leftPosition, topPosition, buttonWidth, AOButtonHeight);
+        UIButton *button = [[UIButton alloc] initWithFrame:frame];
+        button.backgroundColor = [self colorForButtonWithIndex:i];
+        [self.view addSubview:button];
+        
+        leftPosition += buttonWidth + AOButtonSpacing;
     }
 }
 
@@ -59,18 +80,39 @@ static CGFloat const AOCellSpacing = 1.0;
 
 #pragma mark - Utilities
 
+- (CGFloat)cellSize {
+    return (CGRectGetWidth(UIScreen.mainScreen.bounds) - AOFieldMargin * 2) / AOFieldSize - AOCellSpacing;
+}
+
 - (UIColor *)generateRandomColor {
-    switch (arc4random_uniform(5) % 5) {
+    switch (arc4random_uniform(AOButtonCount) % AOButtonCount) {
         case 0:
-            return [UIColor redColor];
+            return [UIColor yellowColor];
         case 1:
-            return [UIColor whiteColor];
+            return [UIColor redColor];
         case 2:
             return [UIColor blueColor];
         case 3:
             return [UIColor greenColor];
         case 4:
+            return [UIColor whiteColor];
+        default:
+            return [UIColor grayColor];
+    }
+}
+
+- (UIColor *)colorForButtonWithIndex:(NSUInteger)index {
+    switch (index) {
+        case 0:
             return [UIColor yellowColor];
+        case 1:
+            return [UIColor redColor];
+        case 2:
+            return [UIColor blueColor];
+        case 3:
+            return [UIColor greenColor];
+        case 4:
+            return [UIColor whiteColor];
         default:
             return [UIColor grayColor];
     }
