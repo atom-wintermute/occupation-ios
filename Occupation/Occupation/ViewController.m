4185f3f54ae5afb042b46069f3b0047697df8bfb
Nikulin Max <minikulin@avito.ru>
2017-05-17 21:54:26 +0300

    Add cells generation.

--- Occupation/Occupation/ViewController.m
+++ Occupation/Occupation/ViewController.m
@@ -8,22 +8,72 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static CGFloat const AONavigationBarHeigth = 44.0;
+static CGFloat const AOStatusBarHeight = 20.0;
 
-@end
+static NSUInteger const AOFieldSize = 18;
+static CGFloat const AOFieldMargin = 10.0;
+static CGFloat const AOCellSpacing = 1.0;
 
 @implementation ViewController
 
+#pragma mark - Live cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupView];
+    [self setupCells];
 }
 
+#pragma mark - View setup
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupView {
+    self.view.backgroundColor = [UIColor blackColor];
 }
 
+- (void)setupCells {
+    CGFloat cellSize = (CGRectGetWidth(UIScreen.mainScreen.bounds) - AOFieldMargin * 2) / AOFieldSize - AOCellSpacing;
+    
+    CGFloat leftPosition = AOFieldMargin;
+    CGFloat topPosition = AOFieldMargin + AOStatusBarHeight;
+    
+    for (NSUInteger i = 0; i < AOFieldSize; ++i) {
+        leftPosition = AOFieldMargin;
+        for (NSUInteger j = 0; j < AOFieldSize; ++j) {
+            CGRect frame = CGRectMake(leftPosition, topPosition, cellSize, cellSize);
+            UIView *cell = [[UIView alloc] initWithFrame:frame];
+            cell.backgroundColor = [self generateRandomColor];
+            [self.view addSubview:cell];
+            leftPosition += cellSize + AOCellSpacing;
+        }
+        topPosition += cellSize + AOCellSpacing;
+    }
+}
+
+#pragma mark - Appearance
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Utilities
+
+- (UIColor *)generateRandomColor {
+    switch (arc4random_uniform(5) % 5) {
+        case 0:
+            return [UIColor redColor];
+        case 1:
+            return [UIColor whiteColor];
+        case 2:
+            return [UIColor blueColor];
+        case 3:
+            return [UIColor greenColor];
+        case 4:
+            return [UIColor yellowColor];
+        default:
+            return [UIColor grayColor];
+    }
+}
 
 @end
